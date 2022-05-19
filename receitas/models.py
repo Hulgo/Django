32bf61f75f35f0734bc7310cf1441cb10e153abf
Hulgo Leonardo J. Andrade <hulgoleo@gmail.com>
2022-05-19 16:24:41 -0300

    Conclusão do Módulo 2

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from turtle import mode
 from django.db import models
 from datetime import datetime
@@ -12,4 +13,7 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
+    foto_receita = models.ImageField(upload_to = 'fotos/%d/%m/%Y/', blank = True)
     publicada = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nome_receita
