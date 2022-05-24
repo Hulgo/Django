cb690a5de64b682be369162994850154c96bf57f
Hulgo Leonardo J. Andrade <hulgoleo@gmail.com>
2022-05-24 16:41:21 -0300

    Continuação Módulo 3 - Django - Alura

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome nào pode ficar em branco!')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo email nào pode ficar em branco!')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não são iguais!')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado!')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save
+        print('Usuário criado com sucesso')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            print('Os campos E-mail e Senha nào podem estar em branco!')
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout(request):
     pass
 
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
